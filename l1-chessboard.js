4f3ba52c8fc8a687e64dbe33d3c5e90c4de24adc
Victor Laforga <37700441+victorlaforga@users.noreply.github.com>
2019-02-07 04:38:16 +0100

    Update l1-chessboard.js

--- l1-chessboard.js
+++ l1-chessboard.js
@@ -1,10 +1,9 @@
-var size = 8;
+var sizeBoard = 8;
 
-var board = ""; //this is the empty string we're going to add either ' ' , '#' or newline
-
-for (var y = 0; y < size; y++) { /*in the outer loop we add newline to seperate rows*/
-    for (var x = 0; x < size; x++) { /*every inner loop rappresents a line, and alternatively it's adding either ' ' or '#' to the string that's being populated*/
-        if ((x + y) % 2 == 0)
+var board = ""; 
+for (var x = 0; x < size; x++) { 
+    for (var p = 0; p < size; p++) { 
+        if ((p + x) % 2 === 0)
             board += " ";
         else
             board += "#";
