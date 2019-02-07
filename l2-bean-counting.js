f1d03fbdfc8333b365bbcbd7ac7507ab9ed2f94a
Victor Laforga <37700441+victorlaforga@users.noreply.github.com>
2019-02-07 04:29:07 +0100

    Update l2-bean-counting.js

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,24 @@
- 
+ function countBs(string) {
+    let letterB = 'B';
+    let count = 0;
+    for (let x = 0; x < string.length; x++) {
+        if (string[x] === letterB);
+        count++;
+    }
+    return count;
+}
+
+function countChar(string, gezochteLetter) {
+    let count = 0;
+    for (let x = 0; x < string.lentgh; x++) {
+        if (string[x] === gezochteLetter);
+        count++;
+    }
+    return count;
+}
+// functie countChar heb ik niet werkend gekregen..
+
+console.log(countBs("BBBbbbbbBC"));
+// → 10
+console.log(countChar("kakkerlak", "k"));
+// → 0
