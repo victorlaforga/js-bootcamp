7303ab622fc44c163c73d078d7d403570befc39b
Victor Laforga <37700441+victorlaforga@users.noreply.github.com>
2019-02-11 18:05:16 +0100

    Update l2-sum-of-a-range.js

--- l2-sum-of-a-range.js
+++ l2-sum-of-a-range.js
@@ -1,1 +1,20 @@
- 
+
+let arrayNew = [];
+function range(start, end) {
+  
+    for (let i = start; i <= end; i++) {
+        arrayNew.push(i);
+ }
+  return arrayNew
+}
+
+// functie om de array bij elkaar op te tellen
+function sum(x){
+  var totaal = 0;
+  for (i=0; i < x.length; i++) {
+  totaal = totaal + x[i];
+  }
+  
+  return totaal;
+
+}
